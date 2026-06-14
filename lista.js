d438d9f77bf7c4d1f887aa310e07ef4489b54542
Vasco <45953077+seabrapaiva@users.noreply.github.com>
2026-06-14 20:34:36 +0200

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,17 @@
+const itensGaleria = [
+    { "arquivo": "itm (1).jpg", "nome": "Chavenas café", "name": "", "disponivel": true },
+    { "arquivo": "itm (2).jpg", "nome": "Tapete de treino", "name": "", "disponivel": true },
+    { "arquivo": "itm (3).jpg", "nome": "Smart TV 32'", "name": "", "disponivel": true },
+    { "arquivo": "itm (4).jpg", "nome": "Torradeira", "name": "", "disponivel": true },
+    { "arquivo": "itm (5).jpg", "nome": "Panela", "name": "", "disponivel": true },
+    { "arquivo": "itm (6).jpg", "nome": "Terrina Pirex", "name": "", "disponivel": true },
+    { "arquivo": "itm (7).jpg", "nome": "Figideira", "name": "", "disponivel": true },
+    { "arquivo": "itm (8).jpg", "nome": "Panela P", "name": "", "disponivel": true },
+    { "arquivo": "itm (9).jpg", "nome": "Candeeiro", "name": "", "disponivel": true },
+    { "arquivo": "itm (10).jpg", "nome": "Panela M", "name": "", "disponivel": true },
+    { "arquivo": "itm (11).jpg", "nome": "Panela G", "name": "", "disponivel": true },
+    { "arquivo": "itm (13).jpg", "nome": "Ambientador", "name": "", "disponivel": true },
+    { "arquivo": "itm (14).jpg", "nome": "Taças", "name": "", "disponivel": true },
+    { "arquivo": "itm (16).jpg", "nome": "Chaleira", "name": "", "disponivel": true },
+    { "arquivo": "itm (17).jpg", "nome": "Tostadeira", "name": "", "disponivel": true }
+];
